ad105659d6ca4eba63b6759dee31e014bbcb498a
Nathaniel Chan <nate.chan29@gmail.com>
2024-02-10 17:12:26 -0500

    feat: point edit links to repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SevenNateNine/DividedByEight',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SevenNateNine/DividedByEight',
         },
         theme: {
           customCss: './src/css/custom.css',
